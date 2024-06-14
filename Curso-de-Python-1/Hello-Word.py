3684baf4474498ef647c2293633ed780f98ffe77
TFabri970sw <154623272+TFabri970sw@users.noreply.github.com>
2024-06-14 14:22:33 -0300

    Nunca desistir

--- Curso-de-Python-1/Hello-Word.py
+++ Curso-de-Python-1/Hello-Word.py
@@ -1,7 +1,25 @@
-print('Hello Word!')
+#Exibir uma mensagem no terminal.
+print('Hello Word!\n')
 print('Hi, My name is Thiago Fabri')
 print('Im 17 years old')
 print('Today is a thursday')
 print('Saint Anthony holiday')
 print('Today is the first day of Python studies')
 print('wish me luck')
+
+#Permite pular uma linha.
+#Se colocar na frente da palavra pula uma linha para cima é no final da palavra pula uma linha para baixo.
+#Exp...
+print('\nPular linha cima')
+print('Pular linha baixo\n')
+print('Exenplo')
+
+#Permite que o usuario esscreva no terminal.
+input()
+
+#As variáveis são utilizadas para armazenar dados durante a execução de um programa.
+#Elas atuam como "caixas" que podem conter diferentes tipos de informações, como números, textos, listas, entre outros.
+#Atribuição de valores: Para criar uma variável, basta atribuir um valor a ela usando o operador de atribuição =. Por exemplo: nome = "Thiago".
+nome = 'Thiago'
+#usar letras minúsculas e separar as palavras com underscores (_). Isso torna o código mais legível e fácil de entender.
+nome_e_sobrenome = 'Thiago Fabri'
---
+++ Sabor-Express/Projeto.py
@@ -0,0 +1,8 @@
+print('Sabor-Express\n')
+
+print('1. Cadastrar-Restaurantes')
+print('2. Listar-Restaurantes')
+print('3. Ativar-Restaurantes')
+print('4. Sair')
+
+opcao_escolhida = input('\nEscolha uma opção: ')
