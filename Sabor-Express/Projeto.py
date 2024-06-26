b7d289948f47a2db15864429dd5079b9014064f4
TFabri970sw <154623272+TFabri970sw@users.noreply.github.com>
2024-06-26 19:48:24 -0300

    try e except

--- Sabor-Express/Projeto.py
+++ Sabor-Express/Projeto.py
@@ -27,21 +27,33 @@ def finalizar_app():
     os.system('cls')
     #os.system('clear') no mac
     print('Encerrando o app...\n')
+
+def opcao_invalida():
+    print('Opcão inválida\n')
+    input('Digite uma tecla para volta ao menu principal')
+    main()
+
     
 def escolher_opcaoes():
-    opcao_escolhida =int(input('\nEscolha uma opção: '))
-    #opcao_escolhida = int(opcao_escolhida)
+    try:
+        opcao_escolhida =int(input('\nEscolha uma opção: '))
+        #opcao_escolhida = int(opcao_escolhida)
 
-    if opcao_escolhida == 1:
-        print('Cadastrar restaurante')
-    elif opcao_escolhida == 2:
-        print('Listar restaurantes')
-    elif opcao_escolhida == 3:
-        print('Ativar restaurante')
-    else:
-        finalizar_app()
+        if opcao_escolhida == 1:
+            print('Cadastrar restaurante')
+        elif opcao_escolhida == 2:
+            print('Listar restaurantes')
+        elif opcao_escolhida == 3:
+            print('Ativar restaurante')
+        elif opcao_escolhida == 4:
+            finalizar_app()
+        else:
+            opcao_invalida()
+    except:
+        opcao_invalida()
 
 def main ():
+    os.system('cls')
     exibir_nome_do_programa()
     exibir_apcoes()
     escolher_opcaoes()
